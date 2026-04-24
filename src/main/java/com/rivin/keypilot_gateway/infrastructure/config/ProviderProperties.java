6407e5acfd6e88c947bffc06124a63f3fd36d8cf
Riviya <rivindu.perera3@gmail.com>
2026-04-24 15:33:55 +0530

    Added @Componenet annotation in ProviderProperties.

--- src/main/java/com/rivin/keypilot_gateway/infrastructure/config/ProviderProperties.java
+++ src/main/java/com/rivin/keypilot_gateway/infrastructure/config/ProviderProperties.java
@@ -2,11 +2,13 @@ package com.rivin.keypilot_gateway.infrastructure.config;
 
 import com.rivin.keypilot_gateway.infrastructure.proxy.auth.AuthType;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
+@Component
 @ConfigurationProperties(prefix = "keypilot")
 public class ProviderProperties {
 
